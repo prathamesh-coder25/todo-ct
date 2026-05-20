46a43267c8bc0f347c92a382b896a649a265fbd3
prathamesh-coder25 <prathshiv2024@gmail.com>
2026-05-20 16:59:50 +0530

    Initial Project Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodoDashboardComponent } from './shared/components/todo-dashboard/todo-dashboard.component';
+import { TodoFormComponent } from './shared/components/todo-form/todo-form.component';
+import { TodoListComponent } from './shared/components/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoDashboardComponent,
+    TodoFormComponent,
+    TodoListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
